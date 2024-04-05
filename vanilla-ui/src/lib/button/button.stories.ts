381daa4c5917795367934b57e863dd08ed859817
Stefan Haas <stefan.haas.privat@gmail.com>
2024-04-05 17:28:29 +0200

    feat: button.ts
    - improved button component
    - improved button story

--- vanilla-ui/src/lib/button/button.stories.ts
+++ vanilla-ui/src/lib/button/button.stories.ts
@@ -5,16 +5,62 @@ import './button';
 
 export default {
   title: 'Button',
-  parameters: {
-    layout: 'centered',
-  },
+
   render: (args) => html`
-    <vui-button label='${args['label']}'></vui-button>`,
+    <vui-button
+      @click=${() => alert('Button clicked!')}
+      label='${args['label']}'
+      ?loading="${args['loading'] ?? false}"
+      loading-spinner="${args['loadingSpinner'] ?? undefined}"
+      icon="${args['icon'] ?? undefined}"
+      icon-pos="${args['iconPos'] ?? undefined}"
+      ?disabled="${args['disabled'] ?? false}"
+    ></vui-button>`
 } as Meta;
 
 export const Default: StoryObj = {
   name: 'Default',
   args: {
-    label: 'Lit',
-  },
+    label: 'Default',
+    disabled: false,
+    loading: false,
+    loadingSpinner: '💩',
+    icon: undefined,
+    iconPos: 'left'
+  }
+};
+
+export const Loading: StoryObj = {
+  name: 'Loading',
+  args: {
+    label: 'Loading...',
+    loadingSpinner: '💩',
+    loading: true,
+    disabled: false,
+    icon: '',
+    iconPos: 'left'
+  }
+};
+
+export const Disabled: StoryObj = {
+  name: 'Disabled',
+  args: {
+    label: 'Disabled',
+    disabled: true,
+    loading: false,
+    loadingSpinner: '💩',
+    icon: '',
+    iconPos: 'left'
+  }
+};
+
+export const WithIcon: StoryObj = {
+  name: 'With icon',
+  args: {
+    label: 'With icon',
+    icon: '🚀',
+    iconPos: 'left',
+    loading: false,
+    loadingSpinner: '💩'
+  }
 };
--- vanilla-ui/src/lib/button/button.ts
+++ vanilla-ui/src/lib/button/button.ts
@@ -1,23 +1,69 @@
 import { html, css, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { Nullable } from 'vitest';
+
+export type IconPosition =
+  | 'left'
+  | 'right';
 
 @customElement('vui-button')
 export class SimpleGreeting extends LitElement {
-  static override styles = css`p {
-    color: blue
-  }`;
+  static override styles = css`
+    .vui-button {
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      gap: 0.5rem;
+    }
+
+    .vui-button-loading-spinner {
+      animation: spin 1s linear infinite;
+    }
+
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  `;
 
   @property() label!: string;
+  @property({ type: Boolean }) loading: Nullable<boolean> = null;
+  @property({ attribute: 'loading-spinner' }) loadingSpinner: Nullable<unknown> = null;
   @property() icon?: unknown;
-  @property() iconPos?: 'left' | 'right' = 'left';
-  @property() disabled = false;
+  @property({ attribute: 'icon-pos' }) iconPos?: IconPosition = 'left';
+  @property({ type: Boolean }) disabled = false;
 
   override render() {
+    const loadingTemplate = html`${
+      this.loading
+        ? html`
+          <div class="vui-button-loading-spinner">${this.loadingSpinner}</div>`
+        : ''
+    }`;
+
+    const iconTemplate = html`${
+      this.icon && !this.loading
+        ? html`
+          <div class="vui-button-icon">${this.icon}</div>`
+        : ''
+    }
+    `;
+
     return html`
       <button
-        type='button'
+        type="button"
+        class="vui-button"
+        ?disabled=${this.disabled || this.loading}
       >
-        ${this.label}
+        ${loadingTemplate}
+
+        ${iconTemplate}
+
+        <div class="vui-button-label">${this.label}</div>
       </button>
     `;
   }
